7f513c1aad24bf6fafe0b94f6a1d1f660f60ed85
Saint-Jean Germila <germila@noukod.com>
2019-03-22 16:22:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,36 @@ function produceTipCalculator( )
 driverId = 0;
 class Driver{
   constructor(name){
-    this.name = name;
+    this.name = name;function produceDrivingRange(blockRange) {
+  return function (startingBlock, endingBlock) {
+    let start = parseInt(startingBlock);
+    let end = parseInt(endingBlock);
+    let distance = Math.abs(end - start);
+    let difference = distance - blockRange;
+
+    if (difference > 0) {
+      return `${Math.abs(difference)} blocks out of range`
+    } else {
+      return `within range by ${Math.abs(difference)}`
+    }
+  }
+}
+
+function produceTipCalculator(percentTip) {
+  return function(fare) {
+    return fare * percentTip
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.name = name
+      this.id = ++ driverId;
+    }
+  }
+}
     this.id = ++driverId;
   }
 }
